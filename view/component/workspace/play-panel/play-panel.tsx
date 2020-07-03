e0daeb33f27fc477c285e1fbd59c5c6092affb3a
Daniel Avedis Masotti <32687396+DanMasotti@users.noreply.github.com>
2020-07-03 15:36:05 -0400

    Revert "play panel follows mouse"

--- view/component/workspace/play-panel/play-panel.tsx
+++ view/component/workspace/play-panel/play-panel.tsx
@@ -8,8 +8,6 @@ import { MetronomeSetting } from "./metronome-setting";
 
 import * as styles from "./play-panel.less";
 import Timecode from "./timecode";
-import { useHover } from "../../../react-hooks";
-import { useEffect, useState } from "react";
 
 const PlayPanel: React.FunctionComponent = () => {
   const metronomeOptions = [
@@ -23,22 +21,8 @@ const PlayPanel: React.FunctionComponent = () => {
     }
   ];
 
-  const [width, setWidth] = useState(-1);
-
-  const [hoverRef, isHovered] = useHover();
-
-  useEffect(() => {
-    if (isHovered) {
-      setWidth(hoverRef.current ? hoverRef.current.offsetWidth : -1);
-    } else {
-      setWidth(-1);
-    }
-  }, [isHovered]);
-
-  const style = width == -1 ? {} : { width };
-
   return (
-    <div className={styles["play-panel"]} ref={hoverRef} style={style}>
+    <div className={styles["play-panel"]}>
       <PlayStopButton></PlayStopButton>
       <Timecode></Timecode>
       <div className={styles["play-options"]}>
