d29a2a3b5fe9b2915cb01f9e3169cdef292cc242
Daniel M <danielmasotti@gmail.com>
2020-07-15 13:00:18 -0400

    Cleaned up speech recognition component, mirroring how vamp audio context works

---
+++ view/component/wrapper/speech-control-wrapper.tsx
@@ -0,0 +1,133 @@
+import * as React from "react";
+import { useMutation, useQuery } from "react-apollo";
+import {
+  PLAY_CLIENT,
+  PAUSE_CLIENT,
+  STOP_CLIENT,
+  RECORD_CLIENT,
+  SEEK_CLIENT
+} from "../../queries/vamp-mutations";
+import { PLAYING_CLIENT, RECORDING_CLIENT } from "../../queries/vamp-queries";
+import {
+  PlayClient,
+  StopClient,
+  RecordClient,
+  Seek,
+  PauseClient,
+  PlayingClient,
+  RecordingClient
+} from "../../state/apollotypes";
+import { useCurrentVampId } from "../../react-hooks";
+import { useEffect, useState } from "react";
+import { vampSpeechRecognizer } from "./vamp-speech-recognizer";
+
+interface SpeechControlTypes {
+  children: React.ReactNode;
+}
+
+export const SpeechControl: React.FC<SpeechControlTypes> = (
+  props: SpeechControlTypes
+): JSX.Element => {
+  const startSpeechRecognition = (): SpeechRecognition => {
+    try {
+      return vampSpeechRecognizer.getRecognition();
+    } catch (e) {
+      alert("Web speech not supported in this browser (TODO)");
+    }
+  };
+
+  const recognition = startSpeechRecognition();
+
+  const vampId = useCurrentVampId();
+  const [play] = useMutation<PlayClient>(PLAY_CLIENT);
+  const [pause] = useMutation<PauseClient>(PAUSE_CLIENT);
+  const [stop] = useMutation<StopClient>(STOP_CLIENT);
+  const [record] = useMutation<RecordClient>(RECORD_CLIENT);
+  const [seek] = useMutation<Seek>(SEEK_CLIENT);
+  const { data, loading, error } = useQuery<PlayingClient>(PLAYING_CLIENT, {
+    variables: { vampId }
+  });
+  const { data: recordingData } = useQuery<RecordingClient>(RECORDING_CLIENT, {
+    variables: { vampId }
+  });
+
+  const [isListening, setIsListening] = useState(false);
+
+  // wake word detection
+  const transcriptHelper = (transcript: string): string => {
+    const transcriptList = transcript.split(" ");
+    const wakeWordIndex = transcriptList.indexOf(
+      vampSpeechRecognizer.getAssistantName()
+    );
+
+    if (wakeWordIndex === -1) {
+      return null;
+    } else {
+      transcript = transcriptList
+        .slice(wakeWordIndex + 1, transcript.length)
+        .join(" ");
+
+      return transcript;
+    }
+  };
+
+  // result from intent of the transcript
+  const voiceCommand = (transcript: string): void => {
+    /*
+      For now, we'll use if statements here, eventually we'll want to use 
+      more sophisticated NLP for Named entity recog -> intents, so we would send the transcript 
+      to an API to do that
+      */
+    if (transcript.includes("play")) {
+      if (!data.vamp.playing) {
+        play();
+      }
+    } else if (transcript.includes("stop")) {
+      if (data.vamp.playing) {
+        if (recordingData.vamp.recording) {
+          seek({ variables: { time: 0 } });
+        } else {
+          stop();
+        }
+      }
+    } else if (transcript.includes("pause")) {
+      console.log("pause!");
+    } else if (transcript.includes("record")) {
+      console.log("record!");
+    }
+  };
+
+  // the recognition occurs here
+  const handleSpeech = (): void => {
+    recognition.onnomatch = (): void => {
+      console.log("sorry I didn't get that");
+    };
+    recognition.onresult = (e: SpeechRecognitionEvent): boolean => {
+      let transcript = e.results[e.results.length - 1][0].transcript;
+      // console.log(transcript);
+
+      transcript = transcriptHelper(transcript);
+
+      if (!transcript) {
+        setIsListening(false);
+        return false;
+      }
+      voiceCommand(transcript);
+      setIsListening(true);
+      return true;
+    };
+    // web speech times out even in continuous mode, here's the fix
+    recognition.onend = (): void => {
+      recognition.start();
+    };
+  };
+
+  useEffect(() => {
+    handleSpeech();
+  });
+
+  // maybe we'd like to render something if we're listening
+  useEffect(() => {}, [isListening]);
+
+  return <div className={"speech-control"}>{props.children}</div>;
+};
---
+++ view/component/wrapper/vamp-speech-recognizer.ts
@@ -0,0 +1,52 @@
+import jspeech from "jspeech";
+
+/*
+Wrapper class for web speech API's recognizer
+*/
+
+class VampSpeechRecognizer {
+  private _assistantName: string;
+  private _recognition: SpeechRecognition;
+
+  constructor() {
+    this._assistantName = "Vivian";
+
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    this._recognition = new SpeechRecognition();
+
+    this._recognition.grammars = this.getGrammar();
+
+    this._recognition.lang = "en-US";
+    this._recognition.continuous = true;
+    this._recognition.start();
+  }
+
+  getGrammar = (): SpeechGrammarList => {
+    const SpeechGrammarList =
+      window.SpeechGrammarList || window.webkitSpeechGrammarList;
+    const speechRecognitionList = new SpeechGrammarList();
+
+    const grammar = jspeech("wakeword", {
+      version: "V1.0",
+      lang: "en",
+      encoding: "utf-8"
+    });
+    grammar.public.rule("greeting", "(a | hey | yo | ayo | <VOID> | <NULL> )");
+    grammar.public.rule("command", "(play | stop | record | pause | loop )");
+    grammar.public.rule(
+      "wake",
+      "<greeting> " + this._assistantName + " <command>"
+    );
+    speechRecognitionList.addFromString(grammar.stringify(), 1);
+
+    return speechRecognitionList;
+  };
+
+  getAssistantName = (): string => this._assistantName;
+
+  getRecognition = (): SpeechRecognition => this._recognition;
+}
+
+// eslint-disable-next-line prefer-const
+export let vampSpeechRecognizer = new VampSpeechRecognizer();
