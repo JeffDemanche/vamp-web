e6f204ddfc257e161085ac4b007771349055bc9d
Jeff Demanche <jeffrey_demanche@alumni.brown.edu>
2021-04-02 19:43:10 -0400

    Removes Redux completely

--- view/app.js
+++ view/app.js
@@ -1,17 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-
-import store from "./redux/store/index";
 
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { VampApp } from "./component/vamp-app";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <VampApp />
-  </Provider>,
-  document.getElementById("app")
-);
+ReactDOM.render(<VampApp />, document.getElementById("app"));
